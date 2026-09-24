6fb459b19ca81ee4cebf9a0fa43cbf52e2bd1876
Aliyyah <aliyyah861@gmail.com>
2026-09-24 10:08:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ const studentTable = document.getElementById("studentTable")
+const studentCount = document.getElementById("studentCount")
+const courseCount = document.getElementById("Coursecount")
+document.getElementById("")
+
+ document.getElementById("t")
